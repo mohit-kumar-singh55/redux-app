0e1d7161ba9e970ec474bf9b614ea30f8c16a9f6
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2021-12-15 19:20:28 +0530

    set up for understanding Redux

---
+++ src/Shop.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Shop() {
+    return (
+        <div>
+            <h1>Buy Kawasaki Ninja 500 100Rs</h1>
+            <button className="btn btn-outline-primary mx-2">-</button>
+            Add to cart
+            <button className="btn btn-outline-primary mx-2">+</button>
+        </div>
+    )
+}
